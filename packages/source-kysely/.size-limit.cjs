c9b394ad03d56488653dcaeff58b75c4829fe27e
devhorse091 <yuyanakai091@gmail.com>
2024-09-09 18:12:33 +0200

    feat: initial source-kysely

--- packages/source-kysely/.size-limit.cjs
+++ packages/source-kysely/.size-limit.cjs
@@ -3,30 +3,10 @@
  * @type {{name: string, path: string[], limit: string, import?: string, webpack?: boolean}[]}
  */
 module.exports = [
-  /*
   {
-    name: "Only { isPlainObject } (ESM)",
+    name: "Only { a } (ESM)",
     path: ["dist/index.mjs"],
-    import: "{ isPlainObject }",
+    import: "{ a }",
     limit: "108B",
   },
-  {
-    name: "Only { assertPlainObject } (ESM)",
-    path: ["dist/index.mjs"],
-    import: "{ assertPlainObject }",
-    limit: "164B",
-  },
-  {
-    name: "Import { assertPlainObject, isPlainObject } (ESM)",
-    path: ["dist/index.mjs"],
-    import: "{ assertPlainObject, isPlainObject }",
-    limit: "171B",
-  },
-  {
-    name: 'Everything (CJS)',
-    import: "*",
-    path: ['dist/index.cjs'],
-    limit: '570KB',
-  },
-  */
 ];
