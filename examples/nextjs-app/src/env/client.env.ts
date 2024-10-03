d15a56ab91466783fd2a3822b648d043fcb780b0
devhorse091 <yuyanakai091@gmail.com>
2024-10-03 11:44:42 +0200

    feat: with sentry

--- examples/nextjs-app/src/env/client.env.ts
+++ examples/nextjs-app/src/env/client.env.ts
@@ -5,9 +5,11 @@ export const clientEnv = createEnv({
   client: {
     // Keep strings as env vars aren't necessarily consumed through t3-env / zod coercion
     NEXT_PUBLIC_ENABLE_HYDRATION_OVERLAY: z.enum(['true', 'false']),
+    NEXT_PUBLIC_SENTRY_ENABLED: z.enum(['true', 'false']),
   },
   runtimeEnv: {
     NEXT_PUBLIC_ENABLE_HYDRATION_OVERLAY:
       process.env.NEXT_PUBLIC_ENABLE_HYDRATION_OVERLAY,
+    NEXT_PUBLIC_SENTRY_ENABLED: process.env.NEXT_PUBLIC_SENTRY_ENABLED,
   },
 });
