46173a6c3eacf52afbac93a8f14db698b5273023
devhorse091 <yuyanakai091@gmail.com>
2024-10-09 20:11:11 +0200

    feat: small wip

---
+++ examples/db-sqlserver/scripts/run-prisma-seeds.ts
@@ -0,0 +1,24 @@
+import { PrismaClientSqlServer } from '../src';
+import { CurrencySeeds } from '../src/seeds/common/currency.seeds';
+
+const prisma = new PrismaClientSqlServer();
+
+async function runPrismaSeeds() {
+  console.log(`Start seeding ...`);
+
+  const companySeeds = new CurrencySeeds(prisma);
+  await companySeeds.execute();
+
+  console.log(`Seeding finished.`);
+}
+
+await runPrismaSeeds()
+  .catch((e) => {
+    console.error(e);
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
+  })
+  // eslint-disable-next-line sonarjs/no-misused-promises
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ examples/db-sqlserver/src/seeds/common/currency.seeds.ts
@@ -0,0 +1,34 @@
+import type { PrismaSqlServer as Prisma } from '../../index';
+import { AbstractSeed } from '../../lib/AbstractSeed';
+import jsonCurrencies from './currencies.json';
+
+const currencyData: Prisma.CurrencyCreateInput[] = jsonCurrencies.map(
+  (currency) => {
+    return {
+      code: currency.code,
+      name: currency.name,
+      name_plural: currency.name_plural,
+      rounding: currency.rounding,
+      display_decimals: currency.decimal_digits,
+      symbol_native: currency.symbol_native,
+      symbol: currency.symbol,
+      createdAt: new Date(),
+      updatedAt: null,
+      withdrawalAt: null,
+    };
+  }
+);
+
+export class CurrencySeeds extends AbstractSeed {
+  execute = async (): Promise<void> => {
+    for (const c of currencyData) {
+      const { code, ...userNonUnique } = c;
+      const inserted = await this.prisma.currency.upsert({
+        where: { code: code! },
+        update: userNonUnique,
+        create: c,
+      });
+      this.log('UPSERT', `Currency ${inserted.code}`);
+    }
+  };
+}
