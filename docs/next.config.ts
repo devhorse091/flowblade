fb45be8881b96b9007aac912e7a1110a73e10d5b
devhorse091 <yuyanakai091@gmail.com>
2025-01-23 16:13:41 +0100

    docs: publish doc website

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,20 +1,24 @@
 import nextra from 'nextra';
 
+/** Useful when you publish the static export in a different basePath, ie gh-pages */
+const basePath = process.env.NEXT_BUILD_ENV_BASE_PATH ?? undefined;
+
 const withNextra = nextra({
   // contentDirBasePath: './src/content'
 });
 
 export default withNextra({
+  ...(basePath ? { basePath } : {}),
   output: 'export',
   images: {
     unoptimized: true, // mandatory, otherwise won't export
   },
+  productionBrowserSourceMaps:
+    process.env.NEXT_BUILD_ENV_SOURCEMAPS !== 'false',
   eslint: {
-    dirs: ['src'],
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: process.env.NEXT_BUILD_ENV_LINT === 'false',
   },
   typescript: {
-    ignoreBuildErrors: true,
-    tsconfigPath: './tsconfig.json',
+    ignoreBuildErrors: process.env.NEXT_BUILD_ENV_TYPECHECK === 'false',
   },
 });
