14cf909b5a3faeccd1dcda9a8bde49c154c2bd7a
devhorse091 <yuyanakai091@gmail.com>
2025-01-23 16:03:18 +0100

    docs: publish doc website

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -5,6 +5,10 @@ const withNextra = nextra({
 });
 
 export default withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true, // mandatory, otherwise won't export
+  },
   eslint: {
     dirs: ['src'],
     ignoreDuringBuilds: true,
